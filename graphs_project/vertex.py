e9ce3507d35f7c2529f823bda68a7a76ff30bca5
Roman <deep.depp@yandex.ru>
2019-09-01 21:20:06 +0300

    added directory with beginnings of graph structure

---
+++ graphs_project/vertex.py
@@ -0,0 +1,14 @@
+
+class Vertex:
+
+    def __init__(self, value):
+        self.value = value
+        self.edges = {}
+
+    def get_edges(self):
+        return list(self.edges.keys())
+
+    def add_edge(self, vertex):
+        print("Adding edge to {}".format(vertex))
+        self.edges[vertex] = True
+        
